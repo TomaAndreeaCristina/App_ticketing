5d08745a40875945550f6e06566055a2d8352fb6
TomaAndreeaCristina <cristina.toma270388@gmail.com>
2024-03-21 19:19:46 +0200

    Initial commit

---
+++ authentication/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from authentication.models import CustomUser
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'departament']
+
+    def save(self, commit=True):
+        self.instance.is_active = False
+        return super().save(commit)
